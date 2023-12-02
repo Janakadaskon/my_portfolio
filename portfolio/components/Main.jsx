4e4c03110c7ad683e7ebbc864cc61d54f3cf24a7
Janaka Daskon <janakadaskon@gmail.com>
2023-12-03 01:07:52 +0530

    set the home

--- portfolio/components/Main.jsx
+++ portfolio/components/Main.jsx
@@ -8,8 +8,14 @@ const Main = () => {
                 <p className='uppercase text-sm tracking-widest text-gray-600'>
                     LET'S BUILD SOMETHING TOGETHER
                 </p>
+                <h1 className='py-2 text-gray-700'>
+                    Hi, I'm <span className='text-[#2d4d93]'>Janaka </span> <span className='text-[#c8a13e]'> Daskon</span>
+                </h1>
                 <h1 className='py-2 text-gray-700'>Full-Stack Developer</h1>
-                <p className='py-4 text-gray-600 max-w-[70%] m-auto'></p>
+                <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
+                Welcome to my digital space, where innovation meets functionality! I'm a passionate and results-driven Full Stack Developer with a knack for transforming ideas into seamless, user-centric experiences. Explore my portfolio to witness the fusion of creativity and technical prowess that defines my journey in the world of web development.
+
+                </p>
             </div>
         </div>
     </div>
